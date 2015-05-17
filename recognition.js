b5ecb9dfb0a8610cf499778986ff004bccffa1ca
DHo123 <Derrickho15@gmail.com>
2015-05-16 19:25:46 -0700

    Last changes didn't completely get committed...

--- recognition.js
+++ recognition.js
@@ -3,97 +3,98 @@
                                  window.webkitSpeechRecognition ||
                                  null;
 	  
-	  var stop = 0;
+	var stop = 0;
 	  
-      if (window.SpeechRecognition === null) {
+    if (window.SpeechRecognition === null) {
         document.getElementById('ws-unsupported').classList.remove('hidden');
         document.getElementById('button-play-ws').setAttribute('disabled', 'disabled');
         document.getElementById('button-stop-ws').setAttribute('disabled', 'disabled');
-      } else {
+    }
+    else {
         var recognizer = new window.SpeechRecognition();
         var transcription = document.getElementById('transcription');
         var log = document.getElementById('log');
-		var snd = new Audio("file.wav"); // buffers automatically when created
+		var snd = new Audio("file.wav");
  
-        // Recogniser doesn't stop listening even if the user pauses
         recognizer.continuous = true;
  
         // Start recognising
         recognizer.onresult = function(event) {
-          transcription.textContent = '';
+        transcription.textContent = '';
 		  
-		  var result;
-          for (var i = event.resultIndex; i < event.results.length; i++) {
+            var result;
+            for (var i = event.resultIndex; i < event.results.length; i++) {
 
-		    result = event.results[i][0].transcript;
-            
-            // Stops recognizing when "stop" is heard
-			if (result.indexOf("stop") > -1 && stop == 0) {
-                recognizer.stop();
-				log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
-				stop = 1;
-            }
-
-            // Gets rid of weird extra whitespace.
-			if (i > 0 && stop == 0) {
-			  console.log("test\n");
-			  result = result.substring(1);
-			}
-			
-            // When result is finalized
-            if (event.results[i].isFinal) {
-              transcription.textContent = result + ' (Confidence: ' + event.results[i][0].confidence + ')';
-              
-                // First word will be some command.
-                var cmd = result.substr(0,result.indexOf(' '));
+                result = event.results[i][0].transcript;
                 
-                if (result.indexOf("print") == 0) {
-                    log.innerHTML = result.substring(6) + '<br />' + log.innerHTML;
+                // Stops recognizing when "stop" is heard
+                if (result.indexOf("stop") > -1 && stop == 0) {
+                    recognizer.stop();
+                    log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
+                    stop = 1;
                 }
-                if (result == "hello") {
-                    alert("Hello to you as well.");
+
+                // Gets rid of weird extra whitespace.
+                if (i > 0 && stop == 0) {
+                  console.log("test\n");
+                  result = result.substring(1);
                 }
-                if (result.indexOf("new tab") > -1) {
-                    window.open();
+                
+                // When result is finalized
+                if (event.results[i].isFinal) {
+                  transcription.textContent = result + ' (Confidence: ' + event.results[i][0].confidence + ')';
+                  
+                    // First word will be some command.
+                    var cmd = result.substr(0,result.indexOf(' '));
+                    
+                    if (result.indexOf("print") == 0) {
+                        log.innerHTML = result.substring(6) + '<br />' + log.innerHTML;
+                    }
+                    if (result == "hello") {
+                        alert("Hello to you as well.");
+                    }
+                    if (result.indexOf("new tab") > -1) {
+                        window.open();
+                    }
+                    if (result.indexOf("go to ") > -1) {
+                        window.open("http://" + result.substring(6), "_blank");
+                    }
                 }
-                if (result.indexOf("go to ") > -1) {
-                    window.open("http://" + result.substring(6), "_blank");
+                else if(stop == 0) {
+                  transcription.textContent += result;
                 }
             }
-            else if(stop == 0) {
-              transcription.textContent += result;
-            }
-          }
         };
  
         // Listen for errors
         recognizer.onerror = function(event) {
-          log.innerHTML = 'Recognition error: ' + event.message + '<br />' + log.innerHTML;
+            log.innerHTML = 'Recognition error: ' + event.message + '<br />' + log.innerHTML;
         };
  
         window.onbeforeunload = function() {
             recognizer.abort();
-            return "Please stop recording.";
+            return "Please be sure you stopped recording.";
         }
  
         document.getElementById('button-play-ws').addEventListener('click', function() {
-          // Set if we need interim results
-          recognizer.interimResults = document.querySelector('input[name="recognition-type"][value="interim"]').checked;
+            // Set if we need interim results
+            recognizer.interimResults = document.querySelector('input[name="recognition-type"][value="interim"]').checked;
  
-          try {
-            recognizer.start();
-			// Play sound when started
-			snd.play();
-			stop = 0;
-            log.innerHTML = 'Recognition started' + '<br />' + log.innerHTML;
-          } catch(ex) {
-            log.innerHTML = 'Recognition error: ' + ex.message + '<br />' + log.innerHTML;
-          }
+            try {
+                recognizer.start();
+                // Play sound when started
+                snd.play();
+                stop = 0;
+                log.innerHTML = 'Recognition started' + '<br />' + log.innerHTML;
+            }
+            catch(ex) {
+                    log.innerHTML = 'Recognition error: ' + ex.message + '<br />' + log.innerHTML;
+            }
         });
  
         document.getElementById('button-stop-ws').addEventListener('click', function() {
-          recognizer.stop();
-          log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
+            recognizer.stop();
+            log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
         });
  
         document.getElementById('clear-all').addEventListener('click', function() {

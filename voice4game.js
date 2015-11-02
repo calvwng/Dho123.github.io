f7ed7588b1af8619550d972aee2b9c46d832a8cc
calvwng <calvwng@gmail.com>
2015-11-02 03:40:09 -0800

    Refactored Voice4Game library to contain speech logic
    * Speech recognition logic is now within the Voice4Game object itself,
    rather than outside the object.
    
    * Transcription and log text destinations are now dynamic (currently can
    be removed or added through init to give user the option of not
    modifying HTML DOM).
    
    * Functions in "textToFunc" mapping can now accept the "result" string
    for parsing, if desired.

--- voice4game.js
+++ voice4game.js
@@ -1,200 +1,180 @@
-// Voice4Game Library
+/**
+* Voice4Game voice command library by Calvin Wong and Stanley Tang
+* with special thanks to Derrick J. Ho and Steven HokSeu Tan
+**/
 window.Voice4Game = (function() {
    var Voice4Game = {};
 
-   /* 
+   /*
    * Maps voice commands to corresponding functions
    * String -> Function
-   */                            
+   */
    Voice4Game.textToFunc = {};
-   
+
+   /*
+   * Prepends HTML child node to document for debugging purposes.
+   * NOTE: Chose to prepend/remove the node based on this field instead of
+   * prepending it unconditionally and showing/hiding it so that the user the
+   * may preserve their HTML's DOM structure if debugging is not needed.
+   */
+   Voice4Game.showDebug = false;
+
+   /* Debug object with containers for transcription and log text */
+   Voice4Game.debug = {
+      transcription: {},
+      log: {}
+   };
+
    /**
    * Initialize the Voice4Game library with a mapping of voice commands to
    * corresponding functions
    */
-   Voice4Game.init = function(textToFuncMap) {
+   Voice4Game.init = function(textToFuncMap, showDebug) {
       Voice4Game.textToFunc = textToFuncMap;
+
+      var doc = document.getElementsByTagName('html')[0];
+      if (!Voice4Game.showDebug && showDebug) { // show debug if it was disabled
+         var debugViewDOM = document.createElement("div");
+         debugViewDOM.innerHTML =
+           "<p>\
+               <strong class=\"important\">Transcription:</strong>\
+               <textarea id=\"transcription\" readonly=\"readonly\"></textarea>\
+               <span id=\"ws-unsupported\" class=\"hidden\">API not supported</span>\
+            </p>\
+            <p>\
+               <strong class=\"important\">Log:</strong>\
+               <div id=\"log\"></div>\
+            </p>";
+         doc.insertBefore(debugViewDOM, doc.firstChild);
+         // Direct debug text to prepended DOM elements
+         Voice4Game.debug.transcription = document.getElementById('transcription');
+         Voice4Game.debug.log = document.getElementById('log');
+      }
+      if (Voice4Game.showDebug && !showDebug) { // remove debug if it was enabled
+         doc.removeChild(doc.childNodes[0]);
+         // Direct debug text to empty objects
+         Voice4Game.debug.transcription = {};
+         Voice4Game.debug.log = {};
+      }
+
+      Voice4Game.showDebug = showDebug; // update showDebug
    };
-   
+
    /**
    * Register a voice command in mapping
    */
    Voice4Game.registerVoiceCmd = function(voiceCmd, cmdFunc) {
       Voice4Game.textToFunc[voiceCmd] = cmdFunc;
    };
-   
+
    /**
    * Unregister a voice command from mapping
    */
    Voice4Game.unregisterVoiceCmd = function(voiceCmd) {
       delete Voice4Game.textToFunc[voiceCmd];
    };
-   
+
+   // Respond to voice commands
+   window.SpeechRecognition = window.SpeechRecognition       ||
+                              window.webkitSpeechRecognition ||
+                              null;
+
+   var stop = 0;
+   if (window.SpeechRecognition === null) {
+       if (Voice4Game.showDebug) {
+          document.getElementById('ws-unsupported').classList.remove('hidden');
+       }
+       // document.getElementById('button-play-ws').setAttribute('disabled', 'disabled');
+       // document.getElementById('button-stop-ws').setAttribute('disabled', 'disabled');
+   }
+   else {
+       var recognizer = new window.SpeechRecognition();
+       // reference debug object (because contents within ref obj may change)
+       var debugRef = Voice4Game.debug;
+       var snd = new Audio("file.wav");
+
+       // Interim results always on for demo purposes.
+       recognizer.interimResults = true;
+
+       recognizer.continuous = false;
+
+       // Start recognising
+       recognizer.onresult = function(event) {
+           debugRef.transcription.textContent = '';
+
+           var result;
+
+           // Loop to find final result.
+           for (var i = event.resultIndex; i < event.results.length; i++) {
+               result = event.results[i][0].transcript;
+
+               // Stops recognizing when "stop" is potentially heard
+               if (result.indexOf("stop") > -1 && stop == 0) {
+                   recognizer.stop();
+                   result = result.substring(0, result.indexOf("stop"));
+                   debugRef.log.innerHTML = 'Recognition stopped' + result + '<br />' + debugRef.log.innerHTML;
+                   stop = 1;
+               }
+
+               // Gets rid of weird extra whitespace.
+               if (i > 0 && stop == 0) {
+                 console.log("test\n");
+                 result = result.substring(1);
+               }
+
+               // When result is finalized
+               if (event.results[i].isFinal) {
+                 debugRef.transcription.textContent = result + ' (Confidence: ' + event.results[i][0].confidence + ')';
+
+                 if (event.results[i][0].confidence < 0.5) {
+                   console.log("voice4game error: confidence too low");
+                 }
+
+                 /*
+                 * Check if result matches any registered Voice4Game commands
+                 */
+                 for (var voiceCmd in Voice4Game.textToFunc) {
+                    if (result.indexOf(voiceCmd) > -1) {
+                       Voice4Game.textToFunc[voiceCmd](result);
+                       break;
+                    }
+                 }
+               }
+               else if(stop == 0) {
+                 debugRef.transcription.textContent += result;
+               }
+           }
+       };
+       // Listen for errors
+       recognizer.onerror = function(event) {
+           debugRef.log.innerHTML = 'Recognition error: ' + event.error + '<br />' + debugRef.log.innerHTML;
+       };
+
+       recognizer.onend = function(event) {
+           if (stop == 0) {
+             recognizer.start();
+           }
+       };
+   }
+
+   Voice4Game.start = function() {
+      try {
+          recognizer.start();
+          // Play sound when started
+          snd.play();
+          stop = 0;
+           debugRef.log.innerHTML = 'Recognition started' + '<br />' + debugRef.log.innerHTML;
+      }
+      catch(ex) {
+           debugRef.log.innerHTML = 'Recognition error: ' + ex.message + '<br />' + debugRef.log.innerHTML;
+      }
+   };
+
+   Voice4Game.stop = function() {
+      stop = 1;
+      recognizer.stop();
+      debugRef.log.innerHTML = 'Recognition stopped' + '<br />' + debugRef.log.innerHTML;
+   };
+
    return Voice4Game;
 }());
-
-/*
-* Mock functions for directional game movement
-*/
-function moveUp() {
-   console.log("moving up");
-}
-function moveDown() {
-   console.log("moving down");
-}
-function moveLeft() {
-   console.log("moving left");
-}
-function moveRight() {
-   console.log("moving right");
-}
-
-/*
-* Mock initialization of Voice4Game
-*/
-Voice4Game.init({
-   'up': moveUp,
-   'down': moveDown,
-   'left': moveLeft,
-   'right': moveRight
-});
-
-// Test browser support
-      window.SpeechRecognition = window.SpeechRecognition       ||
-                                 window.webkitSpeechRecognition ||
-                                 null;
-
-	var stop = 0;
-
-    if (window.SpeechRecognition === null) {
-        document.getElementById('ws-unsupported').classList.remove('hidden');
-        document.getElementById('button-play-ws').setAttribute('disabled', 'disabled');
-        document.getElementById('button-stop-ws').setAttribute('disabled', 'disabled');
-    }
-    else {
-        var recognizer = new window.SpeechRecognition();
-        var transcription = document.getElementById('transcription');
-        var log = document.getElementById('log');
-		var snd = new Audio("file.wav");
-
-        recognizer.continuous = true;
-
-        // Start recognising
-        recognizer.onresult = function(event) {
-        transcription.textContent = '';
-
-            var result;
-            for (var i = event.resultIndex; i < event.results.length; i++) {
-
-                result = event.results[i][0].transcript;
-
-                // Stops recognizing when "stop" is heard
-                if (result.indexOf("stop") > -1 && stop == 0) {
-                    recognizer.stop();
-                    result = result.substring(0, result.indexOf("stop"));
-                    log.innerHTML = 'Recognition stopped' + result + '<br />' + log.innerHTML;
-                    stop = 1;
-                }
-
-                // Gets rid of weird extra whitespace.
-                if (i > 0 && stop == 0) {
-                  console.log("test\n");
-                  result = result.substring(1);
-                }
-
-                // When result is finalized
-                if (event.results[i].isFinal) {
-                  transcription.textContent = result + ' (Confidence: ' + event.results[i][0].confidence + ')';
-
-                    /* Voice commands for scrolling control
-                       1. Scroll to bottom/top over a specified ms duration
-                       2. Scroll down/up by a specified pixel offset */
-                    if (result.indexOf("scroll down over") > -1) {
-                        var durationMatches = result.match(/[0-9]+$/);
-                        if (durationMatches.length > 0) {
-                           var duration = parseInt(durationMatches[0]);
-                           $('html, body').animate({
-                             'scrollTop': $(document).height()
-                           }, duration);
-                        }
-                    }
-                    if (result.indexOf("scroll up over") > -1) {
-                        var durationMatches = result.match(/[0-9]+$/);
-                        if (durationMatches.length > 0) {
-                           var duration = parseInt(durationMatches[0]);
-                           $('html, body').animate({
-                             'scrollTop': 0
-                           }, duration);
-                        }
-                    }
-                    if (result.indexOf("scroll down by") > -1) {
-                        var offsetMatches = result.match(/[0-9]+$/);
-                        if (offsetMatches.length > 0) {
-                           var offset = parseInt(offsetMatches[0]);
-                           $('html, body').animate({
-                             'scrollTop': window.scrollY + offset
-                           }, offset);
-                        }
-                    }
-                    if (result.indexOf("scroll up by") > -1) {
-                        var offsetMatches = result.match(/[0-9]+$/);
-                        if (offsetMatches.length > 0) {
-                           var offset = parseInt(offsetMatches[0]);
-                           $('html, body').animate({
-                             'scrollTop': window.scrollY - offset
-                           }, offset);
-                        }
-                    }
-                    
-                    /*
-                    * Check if result matches any registered Voice4Game commands
-                    */
-                    for (var voiceCmd in Voice4Game.textToFunc) {
-                       if (result.indexOf(voiceCmd) > -1) {
-                          Voice4Game.textToFunc[voiceCmd]();
-                          break;
-                       }
-                    }
-                }
-                else if(stop == 0) {
-                  transcription.textContent += result;
-                }
-            }
-        };
-
-        // Listen for errors
-        recognizer.onerror = function(event) {
-            log.innerHTML = 'Recognition error: ' + event.message + '<br />' + log.innerHTML;
-        };
-
-        window.onbeforeunload = function() {
-            recognizer.abort();
-            return "Please be sure you stopped recording.";
-        }
-
-        document.getElementById('button-play-ws').addEventListener('click', function() {
-            // Set if we need interim results
-            recognizer.interimResults = document.querySelector('input[name="recognition-type"][value="interim"]').checked;
-
-            try {
-                recognizer.start();
-                // Play sound when started
-                snd.play();
-                stop = 0;
-                log.innerHTML = 'Recognition started' + '<br />' + log.innerHTML;
-            }
-            catch(ex) {
-                    log.innerHTML = 'Recognition error: ' + ex.message + '<br />' + log.innerHTML;
-            }
-        });
-
-        document.getElementById('button-stop-ws').addEventListener('click', function() {
-            recognizer.stop();
-            log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
-        });
-
-        document.getElementById('clear-all').addEventListener('click', function() {
-          transcription.textContent = '';
-          log.textContent = '';
-        });
-      }
